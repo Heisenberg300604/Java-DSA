3c0168d950e950f20c75924bfe214926afb97ac8
Nibedan <nibedanpati@gmail.com>
2025-01-23 17:03:27 +0530

    .

--- Strings/src/Palindrome.java
+++ Strings/src/Palindrome.java
@@ -1,14 +1,18 @@
 
 public class Palindrome {
     public static void main(String[] args) {
-
+        String check = "abcba";
+        System.out.println(isPalindrome(check));
     }
     static boolean isPalindrome(String str){
         str = str.toLowerCase();
         for (int i = 0; i < str.length()/2; i++) {
             char start = str.charAt(i);
-            char end = str.charAt(str.length()-1);
-
+            char end = str.charAt(str.length()-1-i);
+            if (start != end) {
+                return false;
+            }
         }
+        return true;
     }
 }
