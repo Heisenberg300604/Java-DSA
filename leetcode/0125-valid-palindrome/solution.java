cf116ac6c2708fdcaa0eee93b03dddfd9a577b5a
Nibedan <nibedanpati@gmail.com>
2025-07-08 16:37:20 +0000

    [LeetCode Sync] Runtime - 3 ms (63.64%), Memory - 43.3 MB (99.93%)

---
+++ leetcode/0125-valid-palindrome/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder cleaned = new StringBuilder();
+         for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(Character.isLetterOrDigit(c)){
+                cleaned.append(Character.toLowerCase(c));
+            }
+        }
+
+        int left = 0;
+        int right = cleaned.length()-1;
+        while(left<right){
+            if(cleaned.charAt(left) != cleaned.charAt(right)){
+                return false;
+            }
+            right--;
+            left++;
+        }
+        return true;
+    }
+}
