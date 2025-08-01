0f3ee107ae4bb572ef77e7c3c0bf778660fe70a9
Nibedan Pati <nibedanpati@gmail.com>
2025-08-02 02:10:07 +0530

    LC 242 valid anagram

---
+++ Leetcode/Problem 242/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+// Valid anagram
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] sSort = s.toCharArray();
+        char[] tSort = t.toCharArray();
+        Arrays.sort(sSort);
+        Arrays.sort(tSort);
+        return Arrays.equals(sSort, tSort);
+    }
+}
