219673ab062bd772124380d4f6908fcb1d09a8ec
Nibedan <nibedanpati@gmail.com>
2025-08-04 19:03:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.9 MB (99.99%)

---
+++ leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    public int firstOccurence(int[] nums,int target){
+        int low =0;
+        int mid;
+        int high = nums.length-1;
+        int first = -1;
+        while(high>= low){
+            mid = (high+low)/2;
+            if(nums[mid]==target){
+                first = mid;
+                high = mid -1;
+            }
+            else if(nums[mid]>target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return first;
+    }
+    public int lastOccurence(int[] nums,int target){
+        int low =0;
+        int mid;
+        int high = nums.length-1;
+        int last = -1;
+        while(high>= low){
+            mid = (high+low)/2;
+            if(nums[mid]==target){
+                last = mid;
+                low = mid +1;
+            }
+            else if(nums[mid]>target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return last;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int first = firstOccurence(nums,target);
+        if(first == -1){
+            return new int[] {-1,-1};
+        }
+        int last = lastOccurence(nums,target);
+        return new int[] {first,last};
+    }
+}
