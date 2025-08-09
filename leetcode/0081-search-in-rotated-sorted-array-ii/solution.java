8a6ba1a82dd7c950f115af63ca44dd1d259916bf
Nibedan <nibedanpati@gmail.com>
2025-08-09 15:03:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.8 MB (99.96%)

---
+++ leetcode/0081-search-in-rotated-sorted-array-ii/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        int mid;
+        while(high>=low){
+            mid = low + (high - low) / 2;
+            if(nums[mid]== target) return true;
+            if(nums[mid] == nums[low] && nums[mid] == nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+            if(nums[mid]>= nums[low]){
+                if(target>=nums[low] && target<nums[mid]){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }else{
+                if(target>nums[mid] && target<=nums[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+}
