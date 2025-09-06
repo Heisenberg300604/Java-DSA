074fb7ee94bec753ecd6cc16c1e63ebb71769413
Nibedan <nibedanpati@gmail.com>
2025-09-06 13:51:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.4 MB (99.96%)

--- leetcode/1791-richest-customer-wealth/solution.java
+++ leetcode/1791-richest-customer-wealth/solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int maxwealth =0;
-        for(int i=0;i<accounts.length;i++){
-            int rowsum=0;
-            for(int j=0;j<accounts[i].length;j++){
-                rowsum += accounts[i][j];
+        int max =0;
+        for(int i= 0 ; i<accounts.length; i++){
+            int sum = 0;
+            for(int j =0 ; j<accounts[i].length;j++){
+                sum+=accounts[i][j];
             }
-            if(rowsum>maxwealth){
-                maxwealth = rowsum;
+            if(sum>max){
+                max = sum;
             }
         }
-        return maxwealth;
+        return max;
     }
 }
