84f2b2b38a1dfae4b2641f994a8748bc91e3d447
Nibedan <nibedanpati@gmail.com>
2025-11-17 20:12:36 +0000

    [LeetCode Sync] Runtime - 3 ms (33.08%), Memory - 49 MB (21.47%)

--- leetcode/0017-letter-combinations-of-a-phone-number/solution.java
+++ leetcode/0017-letter-combinations-of-a-phone-number/solution.java
@@ -1,33 +1,21 @@
 class Solution {
-    public  Map<Integer,String> map=new HashMap<>();
-    public List<String> letterCombinations(String digits) {
-        map.put(2,"abc");
-        map.put(3,"def");
-        map.put(4,"ghi");
-        map.put(5,"jkl");
-        map.put(6,"mno");
-        map.put(7,"pqrs");
-        map.put(8,"tuv");
-        map.put(9,"wxyz");
-        int n = digits.length();
-        List<String> list = new ArrayList<>();
-        if(digits == null || n==0){
-            return list;
-        }
-        backTrack(0 , digits ,new StringBuilder() ,list , map);
-        return list;
-    }
-    private void backTrack(int index ,String digits ,StringBuilder sb,  List<String> list , Map<Integer , String> map ){
-        if(index == digits.length()){
-            list.add(sb.toString());
-            return ;
-        }
-        String first = map.get(digits.charAt(index)-'0');
+     String[] map = new String[]{"","","abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
-        for(char ch:first.toCharArray()){
-            sb.append(ch);
-            backTrack(index+1,digits, sb , list , map);
-            sb.deleteCharAt(sb.length()-1);
+
+     public void helper(String digits, List<String> ans, int index, String current){
+        if(index == digits.length()){
+            ans.add(current);
+            return;
         }
-    };
+        String s = map[digits.charAt(index)-'0'];
+        for(int i =0;i<s.length();i++){
+            helper(digits,ans,index+1,current+s.charAt(i));
+        }
+     }
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+        helper(digits,ans,0,"");
+        return ans;
+
+    }
 }
