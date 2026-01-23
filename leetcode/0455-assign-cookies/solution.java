b2f91cddcd5cc8ec33121baae8ce1c6bb9e35fde
Nibedan <nibedanpati@gmail.com>
2026-01-23 08:02:40 +0000

    [LeetCode Sync] Runtime - 14 ms (92.99%), Memory - 51.8 MB (13.93%)

---
+++ leetcode/0455-assign-cookies/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int n = g.length;
+        int m = s.length;
+        int i = 0 ;
+        int j = 0 ;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        while(i<n && j<m){
+            if(s[j] >= g[i]){
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+}
