f6b475f1f91db3b312f1596d48c635d3a12e9605
Nibedan <nibedanpati@gmail.com>
2026-01-27 20:05:45 +0000

    [LeetCode Sync] Runtime - 45 ms (84.42%), Memory - 115.7 MB (84.03%)

---
+++ leetcode/0435-non-overlapping-intervals/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int comp(int[] val1, int[] val2) {
+        return Integer.compare(val1[1], val2[1]);
+    }
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int n = intervals.length;
+        int count = 1 ;
+        Arrays.sort(intervals, Solution::comp);
+        int lastEndTime = intervals[0][1];
+        for (int i = 1; i < n; i++) {
+            if(intervals[i][0]>=lastEndTime){
+                count++;
+                lastEndTime = intervals[i][1];
+            }
+        }
+        return n-count;
+    }
+}
