18dcb61477b8faf79f5d46ddf2074649345e4dc6
Nibedan <nibedanpati@gmail.com>
2026-03-17 06:54:20 +0000

    [LeetCode Sync] Runtime - 893 ms (7.69%), Memory - 60 MB (18.49%)

--- leetcode/0015-3sum/solution.java
+++ leetcode/0015-3sum/solution.java
@@ -1,30 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> triplets = new ArrayList<>();
+        if(nums == null || nums.length < 3) return new ArrayList<>();
         int n = nums.length;
-        Arrays.sort(nums);
+        Arrays.sort(nums); // sort the ArrayList
 
-        for(int i = 0 ; i<n-2;i++){
+        Set<List<Integer>> result = new HashSet<>();
+
+        for(int i = 0 ; i < n-2 ; i++){
             int j = i+1;
             int k = n-1;
-            if(i>0 && nums[i] == nums[i-1]) continue; // skip duplicates
-            while(k>j){
-                int total = nums[i] + nums[j] + nums[k];
-                if(total > 0 ){
+            while(k> j){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0 ){
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
                     k--;
-                }
-                if(total<0){
+                }else if(sum < 0){
                     j++;
-                }
-                if(total==0){
-                    triplets.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    while (j < k && nums[j] == nums[j + 1]) j++; // skip duplicates
-                    while (j < k && nums[k] == nums[k - 1]) k--;
-                    j++;
+                }else{
                     k--;
                 }
             }
-        }    
-        return triplets;
+        }
+        return new ArrayList<>(result);
     }
 }
