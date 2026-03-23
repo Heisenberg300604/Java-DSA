28a612f0b7c38340f52acb67783a41780410b179
Nibedan <nibedanpati@gmail.com>
2026-03-23 05:43:52 +0000

    [LeetCode Sync] Runtime - 2 ms (90.56%), Memory - 47.7 MB (70.85%)

--- leetcode/0283-move-zeroes/solution.java
+++ leetcode/0283-move-zeroes/solution.java
@@ -1,12 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i =0;
-        for(int j =0;j<nums.length;j++){
-            if(nums[j]!=0){
+        int j = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            // if element is non-zero
+            if(nums[i]!= 0 ){
                 int temp = nums[i];
                 nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
+                nums[j] = temp; // swap the elements
+                j++;
             }
         }
     }
