16d3229de792de34a3ac07f717de655ca879d741
Nibedan <nibedanpati@gmail.com>
2026-03-24 18:31:50 +0000

    [LeetCode Sync] Runtime - 2 ms (93.18%), Memory - 42.9 MB (80.87%)

---
+++ leetcode/0228-summary-ranges/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        int n = nums.length;
+        if(n == 0) return result;
+        for(int i = 0 ; i < n;i++){
+            int start = nums[i];
+            while(i+1 < n && nums[i+1] - nums[i] == 1){ // difference 1 hai
+                i++;
+            }
+            if(start != nums[i]){
+                result.add(start + "->"+nums[i]);
+            }else{
+                result.add(String.valueOf(start));
+            }
+        }
+        return result;
+    }
+}
