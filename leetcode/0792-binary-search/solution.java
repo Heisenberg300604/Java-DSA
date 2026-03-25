ee575755722f89b5cae78a0969bdf7b0694047e4
Nibedan <nibedanpati@gmail.com>
2026-03-25 09:21:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 48.5 MB (43.01%)

--- leetcode/0792-binary-search/solution.java
+++ leetcode/0792-binary-search/solution.java
@@ -1,21 +1,18 @@
 class Solution {
-    private int searching(int[] nums, int target , int start, int end){
-        if(start>end){
-            return -1;
-        }
-        int mid = (start+end)/2;
-        if(nums[mid]==target){
-            return mid;
-        }
-        if(nums[mid]>target){
-            return searching(nums,target,start,mid-1);
-        }else{
-            return searching(nums,target,mid+1,end);
-        }
-    }
     public int search(int[] nums, int target) {
-        int n = nums.length-1;
-        return searching(nums,target, 0, n);
+        int left = 0;
+        int right = nums.length - 1;
         
+        while (right >= left) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
     }
 }
