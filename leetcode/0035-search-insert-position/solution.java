a1afaf37db47344fc29bc8273b650d6ebecf478c
Nibedan <nibedanpati@gmail.com>
2026-04-05 11:46:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.5 MB (93.54%)

--- leetcode/0035-search-insert-position/solution.java
+++ leetcode/0035-search-insert-position/solution.java
@@ -1,16 +1,17 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int low =0;
+        // revising old topics
+        // nums is sorted and we ought to find the index if target found else return index where it would be inserted in order
+        int low = 0 ; 
         int high = nums.length-1;
         int ans = nums.length;
-        int mid;
-        while(high>=low){
-            mid = (high+low)/2;
-            if(nums[mid]>=target){
+        while(high >= low){
+            int mid = low + (high-low)/2;
+            if(nums[mid] >= target ){
                 ans = mid;
                 high = mid-1;
             }else{
-                low = mid+1;
+                low = mid +1;
             }
         }
         return ans;
