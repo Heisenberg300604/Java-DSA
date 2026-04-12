046fa0ae5ebfcf7ab64a7efba585cd51eb2254fa
Nibedan <nibedanpati@gmail.com>
2026-04-12 07:28:01 +0000

    [LeetCode Sync] Runtime - 3 ms (33.82%), Memory - 49.5 MB (5.65%)

--- leetcode/0017-letter-combinations-of-a-phone-number/solution.java
+++ leetcode/0017-letter-combinations-of-a-phone-number/solution.java
@@ -1,21 +1,23 @@
 class Solution {
-     String[] map = new String[]{"","","abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    String[] map = new String[] { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
 
-
-     public void helper(String digits, List<String> ans, int index, String current){
-        if(index == digits.length()){
-            ans.add(current);
+    public void helper(List<String> result, String digits, int index, String current) {
+        if (index == digits.length()) {
+            result.add(current);
             return;
         }
-        String s = map[digits.charAt(index)-'0'];
-        for(int i =0;i<s.length();i++){
-            helper(digits,ans,index+1,current+s.charAt(i));
+        String s = map[digits.charAt(index) - '0'];
+        for (int i = 0; i < s.length(); i++) {
+            helper(result, digits, index + 1, current + s.charAt(i));
         }
-     }
-    public List<String> letterCombinations(String digits) {
-        List<String> ans = new ArrayList<>();
-        helper(digits,ans,0,"");
-        return ans;
+    }
 
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return result;
+        }
+        helper(result, digits, 0, "");
+        return result;
     }
 }
