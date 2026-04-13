845d01170da88a788cc81b3b35af51745a1c55bd
Nibedan <nibedanpati@gmail.com>
2026-04-13 12:04:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.4 MB (99.58%)

---
+++ leetcode/0909-stone-game/solution.java
@@ -0,0 +1,5 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true; // Alice will always win can always force a win by choosing either all even-indexed piles or all odd-indexed piles, whichever has more stones 
+    }
+}
