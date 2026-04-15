cd211d4a4c00fae99d5f66c6e0d35c0856cbf7e7
Nibedan <nibedanpati@gmail.com>
2026-04-15 17:05:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 46.9 MB (36.22%)

---
+++ leetcode/2598-shortest-distance-to-target-string-in-a-circular-array/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int closestTarget(String[] words, String target, int startIndex) {
+        int n = words.length;
+        for (int step = 0; step < n; step++) {
+            int forward = (startIndex + step) % n;
+            int backward = (startIndex - step + n) % n;
+            if (words[forward].equals(target) || words[backward].equals(target)) {
+                return step;
+            }
+        }
+        return -1;
+    }
+}
