e24bbd6c0569fb1927efea7dae1d1108602de650
Nibedan <nibedanpati@gmail.com>
2026-04-18 06:59:27 +0000

    [LeetCode Sync] Runtime - 1 ms (63.61%), Memory - 47.1 MB (61.81%)

---
+++ leetcode/0801-is-graph-bipartite/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean bfs(int start, int[][] graph, int[] color) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        color[start] = 0;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+
+            for (int it : graph[node]) {
+                if (color[it] == -1) {
+                    color[it] = 1 - color[node];
+                    q.add(it);
+                } else if (color[it] == color[node]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isBipartite(int[][] graph) {
+        int V = graph.length;
+        int[] color = new int[V];
+        Arrays.fill(color, -1);
+
+        for (int i = 0; i < V; i++) {
+            if (color[i] == -1) {
+                if (!bfs(i, graph, color)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
