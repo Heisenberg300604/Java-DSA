5709e3552113e96e5e039cba4698d338395b33d9
Nibedan <nibedanpati@gmail.com>
2026-04-19 15:43:40 +0000

    [LeetCode Sync] Runtime - 604 ms (71.82%), Memory - 0.0B (100.00%)

---
+++ leetcode/1882-the-number-of-employees-which-report-to-each-employee/solution.sql
@@ -0,0 +1,10 @@
+select e1.employee_id, 
+e1.name ,
+(count(e2.employee_id)) as reports_count ,
+round((avg(e2.age))) as average_age
+from Employees e1
+left join Employees e2
+on e1.employee_id = e2.reports_to
+GROUP BY e1.employee_id, e1.name
+having count(e2.employee_id) > 0
+order by e1.employee_id;
