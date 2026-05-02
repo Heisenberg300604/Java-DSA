3f66155ce1019bd9913770644364a1584afa88ce
Nibedan <nibedanpati@gmail.com>
2026-05-02 15:36:39 +0000

    [LeetCode Sync] Runtime - 5 ms (99.52%), Memory - 45.6 MB (19.11%)

--- leetcode/0040-combination-sum-ii/solution.java
+++ leetcode/0040-combination-sum-ii/solution.java
@@ -1,35 +1,25 @@
 class Solution {
-    private void func(int ind, int sum, List<Integer> nums, 
-                      int[] candidates, List<List<Integer>> ans) {
-        if (sum == 0) {
-            ans.add(new ArrayList<>(nums));
-            return;
-        }
-        if (sum < 0 || ind == candidates.length) return; 
-
-        nums.add(candidates[ind]);
-
-        // Recursively call with updated sum and next index
-        func(ind + 1, sum - candidates[ind], nums, candidates, ans);
-
-        // Backtrack by removing the last added candidate
-        nums.remove(nums.size() - 1);
-
-        // Skip duplicates
-        for(int i = ind + 1; i < candidates.length; i++) {
-            if(candidates[i] != candidates[ind]) {
-                func(i, sum, nums, candidates, ans);
-                break;
+    public void combine(List<List<Integer>> result, int[] candidates, List<Integer> combination, int target, int sum,
+            int index) {
+                if (sum == target) {
+                result.add(new ArrayList<>(combination));
+                return;
             }
+            if (sum > target) return;
+        for (int i = index; i < candidates.length; i++) { // iterate at the same level
+            if (i > index && candidates[i] == candidates[i - 1])
+                continue;
+            if (sum + candidates[i] > target) break;
+            combination.add(candidates[i]);
+            combine(result, candidates, combination, target, sum + candidates[i], i + 1);
+            combination.remove(combination.size() - 1);
         }
     }
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> nums = new ArrayList<>();
-        // Sort candidates to handle duplicates
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(candidates);
-        func(0, target, nums, candidates, ans);
-        return ans;
+        combine(result, candidates, new ArrayList<>(), target, 0, 0);
+        return result;
     }
 }
