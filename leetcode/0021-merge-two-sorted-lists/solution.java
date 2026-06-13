257271153de85cb1668f9509e2c123ff7df84b6a
Nibedan <nibedanpati@gmail.com>
2026-06-13 07:07:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.2 MB (76.01%)

--- leetcode/0021-merge-two-sorted-lists/solution.java
+++ leetcode/0021-merge-two-sorted-lists/solution.java
@@ -14,18 +14,20 @@ class Solution {
         ListNode current = dummy;
 
         while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
+            if(list1.val <= list2.val){
                 current.next = list1;
-                list1 = list1.next;
-            } else {
+                list1= list1.next;
+            }
+            else{
                 current.next = list2;
                 list2 = list2.next;
             }
             current = current.next;
         }
-        if (list1 != null) {
+        if(list1 !=null){
             current.next = list1;
-        } else {
+        }
+        else{
             current.next = list2;
         }
         return dummy.next;
