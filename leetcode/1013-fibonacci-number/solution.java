1a6a7c976e1466e41ae853c5805395e70a67c8d4
Nibedan <nibedanpati@gmail.com>
2026-06-14 05:12:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.9 MB (86.14%)

--- leetcode/1013-fibonacci-number/solution.java
+++ leetcode/1013-fibonacci-number/solution.java
@@ -1,11 +1,15 @@
 class Solution {
+    public int fibonnaci(int n , int[] dp){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        if(dp[n]!=0){
+            return dp[n]; 
+        }
+        return dp[n] = fibonnaci(n-1 , dp) + fibonnaci(n-2,dp);
+    }
     public int fib(int n) {
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        return fib(n-1)+fib(n-2);
+        int[] dp = new int[n+1];
+        return fibonnaci(n , dp);
     }
 }
