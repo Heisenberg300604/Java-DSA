400a5cf2d68f61f735a62eca3a2082071bcb9f28
Nibedan <nibedanpati@gmail.com>
2026-06-15 05:45:55 +0000

    [LeetCode Sync] Runtime - 3 ms (99.94%), Memory - 202.6 MB (42.87%)

--- leetcode/2216-delete-the-middle-node-of-a-linked-list/solution.java
+++ leetcode/2216-delete-the-middle-node-of-a-linked-list/solution.java
@@ -5,40 +5,26 @@
  *     ListNode next;
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     ListNode(int val, ListNode next) { this.next = next; }
  * }
  */
-
 class Solution {
-    public ListNode middleOfLinkedList(ListNode head) {
-        
-        ListNode middle= null;
-        ListNode current = head;
-        ListNode prev = null;
-        int count = 0;
-        while(current != null){
-            count++;
-            current = current.next;
-        }
-        int mid = count/2;
-        int curr = 0;
-        current = head;
-        while(current != null){
-            if(curr == mid){
-                prev.next = current.next;
-                return head;
-            }
-            curr++;
-            prev = current;
-            current = current.next;
-        }
-
-        return head;
-    }
     public ListNode deleteMiddle(ListNode head) {
-        if(head == null || head.next == null){
+        if (head == null || head.next == null) {
             return null;
         }
-        return middleOfLinkedList(head);
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null; // to reach the node before the middle of the linked list node
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = slow.next;
+        return head;
     }
 }
