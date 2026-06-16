bfca02732dac986dfe3f028992afce0ce10209e1
Nibedan <nibedanpati@gmail.com>
2026-06-16 04:06:18 +0000

    [LeetCode Sync] Runtime - 4 ms (91.84%), Memory - 82.6 MB (78.53%)

--- leetcode/0287-find-the-duplicate-number/solution.java
+++ leetcode/0287-find-the-duplicate-number/solution.java
@@ -1,15 +1,25 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int slow = nums[0], fast = nums[0];
-        do{
+        int slow = nums[0];
+        int fast = nums[0];
+        int n = nums.length;
+        // detect the cycle first
+        slow = nums[slow]; // ek baar move krega
+        fast = nums[nums[fast]]; // do baar move krega
+        while(slow !=fast){
             slow = nums[slow];
             fast = nums[nums[fast]];
-        }while(slow !=fast);
-        slow = nums[0];
-        while(slow != fast){
+        }
+
+        // move fast and slow pointer 1 time it is guaranteed that they will meet atleast once
+        // return the slow or fast pointer
+
+        slow = nums[0]; // entrance of the cycle reset it
+
+        while(slow !=fast){
             slow = nums[slow];
             fast = nums[fast];
         }
-        return fast;
+        return slow;
     }
 }
