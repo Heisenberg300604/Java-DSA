dd6554f2f5fea56a842a03ce88eb8a76b9a39427
Nibedan <nibedanpati@gmail.com>
2026-08-18 07:50:02 +0000

    [LeetCode Sync] Runtime - 45 ms (42.24%), Memory - 46.3 MB (34.72%)

--- leetcode/0300-longest-increasing-subsequence/solution.java
+++ leetcode/0300-longest-increasing-subsequence/solution.java
@@ -1,31 +1,19 @@
 class Solution {
-    public int solve(int i, int prev, int[] nums, int[][] dp) {
-        if (i == nums.length) { // base condition 
-            return 0;
-        }
-        // check if already exists in dp array 
-        if (dp[i][prev+1] != -1) {
-            return dp[i][prev+1];
-        }
-        // just skip nums[i]
-        int skip = solve(i + 1, prev, nums,dp);
-
-        // take nums[i]
-        int take = 0;
-        // condition to take or skip in here
-        if (prev == -1 || nums[i] > nums[prev]) {
-            take = 1 + solve(i + 1, i, nums,dp);
-        }
-
-        return dp[i][prev+1] = Math.max(take, skip);
-    }
-
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n + 1][n + 1];
-        for(int[] i : dp){
-            Arrays.fill(i,-1);
+        int[] t = new int[n];
+        Arrays.fill(t, 1); // every array element has LIS of 1 rn..
+        // global variable to store the maxLIS
+        int maxLIS = 1;
+        /// Compare nums[i] with every previous element
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < i ;j++){
+                if(nums[j] < nums[i]){
+                    t[i] = Math.max(t[i] , t[j] + 1);
+                    maxLIS = Math.max(t[i] , maxLIS);
+                }
+            }
         }
-        return solve(0, -1, nums, dp);
+        return maxLIS;
     }
 }
